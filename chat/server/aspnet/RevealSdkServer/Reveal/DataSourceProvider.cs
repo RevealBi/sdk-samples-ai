086583e841e1f4ce531b7fc0be6a22da7a964d78
Brian Lagunas <835562+brianlagunas@users.noreply.github.com>
2025-12-12 16:01:53 -0700

    working on a chat sample

---
+++ chat/server/aspnet/RevealSdkServer/Reveal/DataSourceProvider.cs
@@ -0,0 +1,18 @@
+﻿using Reveal.Sdk;
+using Reveal.Sdk.Data;
+
+namespace RevealSdkServer.Reveal
+{
+    public class DataSourceProvider: IRVDataSourceProvider
+    {
+        public Task<RVDataSourceItem> ChangeDataSourceItemAsync(IRVUserContext userContext, string dashboardId, RVDataSourceItem dataSourceItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RVDashboardDataSource> ChangeDataSourceAsync(IRVUserContext userContext, RVDashboardDataSource dataSource)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
